419d0e4997ac61b37a4da0cb4bfa40e5df3ff458
William Narmontas <william@scalawilliam.com>
2015-11-13 22:10:21 +0000

    But make it look good at least.

--- www/api/index.php
+++ www/api/index.php
@@ -2,7 +2,7 @@
 require_once("../render.inc.php");
 require("../render_game.inc.php");
 ?>
-    <article id="api">
+    <article id="questions">
         <h2>Query the most recent games</h2>
         <pre><code><a href="http://api.actionfps.com/recent/" rel="nofollow" target="_blank">http://api.actionfps.com/recent/</a></code></pre>
     </article>
