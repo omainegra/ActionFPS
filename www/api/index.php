e2ae09ca5f5985ef0968034260a5eaf4393ac12a
William Narmontas <william@scalawilliam.com>
2015-11-14 07:55:54 +0000

    Update api doc.

--- www/api/index.php
+++ www/api/index.php
@@ -5,6 +5,14 @@ require("../render_game.inc.php");
     <article id="questions">
         <h2>Query the most recent games</h2>
         <pre><code><a href="http://api.actionfps.com/recent/" rel="nofollow" target="_blank">http://api.actionfps.com/recent/</a></code></pre>
+
+        <h2>Query clans</h2>
+        <pre><code><a href="http://api.actionfps.com/clans/json/" rel="nofollow" target="_blank">http://api.actionfps.com/clans/json/</a></code></pre>
+        <pre><code><a href="http://api.actionfps.com/clans/yaml/" rel="nofollow" target="_blank">http://api.actionfps.com/clans/yaml/</a></code></pre>
+
+        <h2>Query users</h2>
+        <pre><code><a href="http://api.actionfps.com/users/" rel="nofollow" target="_blank">http://api.actionfps.com/users/</a></code></pre>
+
     </article>
 
 <?php echo $foot; ?>
