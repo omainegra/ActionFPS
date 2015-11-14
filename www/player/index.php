044682160ba0c724d8dfa3635c3a4423ea79ba32
William Narmontas <william@scalawilliam.com>
2015-11-14 17:29:09 +0000

    Hmm

--- www/player/index.php
+++ www/player/index.php
@@ -5,7 +5,42 @@ require("../render_game.inc.php");
 
 $achievements = json_decode(file_get_contents("http://api.actionfps.com/achievements/" . $_GET['id'] . "/"), true);
 
-function completed_achievement($achievement) {
+function capture_master($maps) {
+    ?><section class="content">
+
+    <div class="master">
+    <table class="map-master">
+    <thead><tr><th>Map</th><th>RVSF</th><th>CLA</th></tr></thead>
+    <tbody>
+    <?php foreach($maps as $map) {
+
+    ?>
+    <tr class="<?php echo $map['completed'] ? "complete" : "incomplete"; ?>">
+        <th>ctf @ <?php echo $map['map']; ?></th>
+    <td class="cla <?php echo $map['cla'] == '3/3' ? 'complete' : 'partial'; ?>"><?php echo $map['cla']; ?></td>
+    <td class="rvsf <?php echo $map['rvsf'] == '3/3' ? 'complete' : 'partial'; ?>"><?php echo $map['rvsf']; ?></td>
+
+</tr>
+<?php } ?>
+</tbody>
+</table>
+</div>
+</section> <?php
+}
+
+function progress_val($percent)
+{
+    if ($percent <= 50) {
+        $rightDeg = round(90 + 3.6 * $percent);
+        return "linear-gradient(90deg, #2f3439 50%, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0)), linear-gradient(${rightDeg}deg, #ff6347 50%, #2f3439 50%, #2f3439);";
+    } else {
+        $leftDeg = round(3.6 * $percent - 270);
+        return "linear-gradient(" . $leftDeg . "deg, #ff6347 50%, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0)), linear-gradient(270deg, #ff6347 50%, #2f3439 50%, #2f3439);";
+    }
+}
+
+function completed_achievement($achievement)
+{
     ?>
     <div class="AchievementCard achievement achieved">
         <div class="cont">
@@ -20,35 +55,49 @@ function completed_achievement($achievement) {
 
                     <p><?php echo $achievement['description']; ?></p>
                 </header>
-                <p class="achieved-on">
+                <?php
+
+                if ( isset($achievement['extra']) && isset($achievement['extra']['maps'])) {
+                    capture_master($achievement['extra']['maps']); }
+    ?>
+                    <p
+} class="achieved-on">
                     <time is="relative-time" datetime="<?php echo $achievement['at']; ?>">
                         <?php echo $achievement['at']; ?>                    </time>
-                    </p>
+                </p>
             </div>
         </div>
     </div>
     <?php
 }
-function progress_achievement($achievement) {
+
+function progress_achievement($achievement)
+{
     ?>
     <div class="AchievementCard achievement notAchieved">
         <div class="cont">
             <div class="achievement-left">
-                <div class="progress-radial progress-<?php echo $achievement['percent']; ?>">
+                <div class="progress-radial" style="background-image: <?php echo progress_val($achievement['percent']); ?>">
                     <div class="overlay"><?php echo $achievement['percent']; ?>%</div>
                 </div>
             </div>
             <div class="achievement-description">
                 <header>
                     <h3><?php echo $achievement['title']; ?></h3>
+
                     <p><?php echo $achievement['description']; ?></p>
                 </header>
+                <?php if ( isset($achievement['extra']) && isset($achievement['extra']['maps'])) {
+                    capture_master($achievement['extra']['maps']); }
+                ?>
             </div>
         </div>
     </div>
     <?php
 }
-function none_achievement($achievement) {
+
+function none_achievement($achievement)
+{
     ?>
     <div class="AchievementCard achievement notAchieved">
         <div class="cont">
@@ -60,6 +109,7 @@ function none_achievement($achievement) {
             <div class="achievement-description">
                 <header>
                     <h3><?php echo $achievement['title']; ?></h3>
+
                     <p><?php echo $achievement['description']; ?></p>
                 </header>
             </div>
@@ -71,23 +121,23 @@ function none_achievement($achievement) {
 ?>
 
     <article id="profile">
-        <div class="profile">
-            <div class="achievements">
-                <h3>Achievements</h3>
+    <div class="profile">
+        <div class="achievements">
+            <h3>Achievements</h3>
 
-                <div class="achievements">
- <?php foreach($achievements['completedAchievements'] as $achievement) {
-    completed_achievement($achievement);
-} ?>
- <?php foreach($achievements['partialAchievements'] as $achievement) {
-     progress_achievement($achievement);
-} ?>
- <?php foreach($achievements['switchNotAchieveds'] as $achievement) {
-     none_achievement($achievement);
-} ?>
-                </div>
+            <div class="achievements">
+                <?php foreach ($achievements['completedAchievements'] as $achievement) {
+                    completed_achievement($achievement);
+                } ?>
+                <?php foreach ($achievements['partialAchievements'] as $achievement) {
+                    progress_achievement($achievement);
+                } ?>
+                <?php foreach ($achievements['switchNotAchieveds'] as $achievement) {
+                    none_achievement($achievement);
+                } ?>
             </div>
         </div>
+    </div>
 
     </article><?php
 echo $foot;
