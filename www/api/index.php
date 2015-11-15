db9965b39dd99758528a211af7ee1d72110075b3
William Narmontas <william@scalawilliam.com>
2015-11-15 16:43:23 +0000

    Add /raw/

--- www/api/index.php
+++ www/api/index.php
@@ -13,6 +13,10 @@ require("../render_game.inc.php");
         <h2>Query users</h2>
         <pre><code><a href="http://api.actionfps.com/users/" rel="nofollow" target="_blank">http://api.actionfps.com/users/</a></code></pre>
 
+        <h2>Query <span style="text-decoration: underline;">ALL GAMES</span></h2>
+        <p>This is a goldmine for data exploration</p>
+        <pre><code><a href="http://api.actionfps.com/raw/" rel="nofollow" target="_blank">http://api.actionfps.com/raw/</a></code></pre>
+
     </article>
 
 <?php echo $foot; ?>
