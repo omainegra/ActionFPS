7a12f720173a47d3a58763f4c5205ba950693bc2
Sanzouille <bonnalq@gmail.com>
2015-12-01 23:05:46 +0100

    server page
    Created server list page

---
+++ www/questions/servers/index.php
@@ -0,0 +1,16 @@
+<?php
+require_once("../render.inc.php");
+?>
+<article id="servers">
+        <h2>AssaultCube</h2>
+        <h3>European (France)</h3>
+        <ul>
+            <li><a href="assaultcube://aura.woop.ac:1999">aura.woop.ac 1999</a></li>
+            <li><a href="assaultcube://aura.woop.ac:2999">aura.woop.ac 2999</a></li>
+            <li><a href="assaultcube://aura.woop.ac:3999">aura.woop.ac 3999</a></li>
+            <li><a href="assaultcube://aura.woop.ac:4999">aura.woop.ac 4999</a></li>
+            <li><a href="assaultcube://aura.woop.ac:1337">aura.woop.ac 1337</a> (Match Client)</li>
+        </ul>
+    </article>
+
+<?php echo $foot; ?>
