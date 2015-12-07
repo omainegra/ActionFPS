11fc405ee5b443c7623501a31fdb1d29703549d9
William Narmontas <william@scalawilliam.com>
2015-12-07 07:03:34 +0000

    Hook it up, it all works now.

--- api/app/services/PingerService.scala
+++ api/app/services/PingerService.scala
@@ -2,9 +2,13 @@ package services
 
 import javax.inject._
 
-import acleague.pinger.{SendPings, Pinger}
-import akka.actor.{Kill, ActorSystem}
+import akka.actor.ActorDSL._
+import acleague.pinger._
+import akka.actor.{Props, Kill, ActorSystem}
 import play.api.inject.ApplicationLifecycle
+import play.api.libs.EventSource.Event
+import play.api.libs.iteratee.Concurrent
+import play.api.libs.json.Json
 
 import scala.concurrent.{Future, ExecutionContext}
 
@@ -17,18 +21,62 @@ class PingerService @Inject()(applicationLifecycle: ApplicationLifecycle,
                              )(implicit actorSystem: ActorSystem,
                                executionContext: ExecutionContext) {
 
-  val pingerActor = actorSystem.actorOf(name = "pinger", props = Pinger.props)
+  val (liveGamesEnum, liveGamesChan) = Concurrent.broadcast[Event]
+
+  val listenerActor = actor(factory = actorSystem, name = "pinger")(new PingerService.ListenerActor({ e =>
+    liveGamesChan.push(e)
+  }))
 
   import concurrent.duration._
 
   val schedule = actorSystem.scheduler.schedule(0.seconds, 5.seconds) {
     recordsService.servers.foreach { server =>
-      pingerActor ! SendPings(server.hostname, server.port)
+      listenerActor ! SendPings(server.hostname, server.port)
     }
   }
 
-  applicationLifecycle.addStopHook(() => Future.successful(pingerActor ! Kill))
+  applicationLifecycle.addStopHook(() => Future.successful(listenerActor ! Kill))
   applicationLifecycle.addStopHook(() => Future.successful(schedule.cancel()))
 
+
+}
+
+object PingerService {
+
+  implicit val spw = Json.writes[ServerPlayer]
+  implicit val stw = Json.writes[ServerTeam]
+  implicit val cgw = Json.writes[CurrentGame]
+  implicit val ssw = Json.writes[ServerStatus]
+  implicit val cgpw = Json.writes[CurrentGamePlayer]
+  implicit val cgtw = Json.writes[CurrentGameTeam]
+  implicit val cgnsw = Json.writes[CurrentGameNowServer]
+  implicit val cgnw = Json.writes[CurrentGameNow]
+  implicit val cgsw = Json.writes[CurrentGameStatus]
+
+  object ListenerActor {
+    def props(g: Event => Unit) = Props(new ListenerActor(g))
+  }
+
+  class ListenerActor(g: Event => Unit) extends Act {
+
+    val pingerActor = context.actorOf(name = "pinger", props = Pinger.props)
+
+    become {
+      case sp: SendPings =>
+        pingerActor ! sp
+      case a: ServerStatus =>
+        g(Event(
+          id = Option(a.server),
+          name = Option("server-status"),
+          data = Json.toJson(a).toString()
+        ))
+      case b: CurrentGameStatus =>
+        g(Event(
+          id = Option(b.now.server.server),
+          name = Option("current-game-status"),
+          data = Json.toJson(b).toString()
+        ))
+    }
+  }
 
 }
