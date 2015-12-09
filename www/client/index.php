ae26651835d0ef5ee298a2ab7238e0d6f8ba3d1b
William Narmontas <me@vynar.com>
2015-12-09 21:13:16 +0000

    Create index.php

---
+++ www/client/index.php
@@ -0,0 +1,8 @@
+<?php
+require("../render.inc.php");
+?>
+<br/><br/>
+<p><a href="http://woop.ac/client/windows_client_1202.2.exe">Download the Woop Clan AssaultCube Match Client v.1.2.0.2.2 (44MB, Windows exe)</a></p>
+<p><a href="/client/changes/">View changelog</a></p>
+<?php
+echo $foot;
