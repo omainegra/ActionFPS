3ad0b99b3766add86af30e0d2b7668d8a28427c2
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2015-12-25 09:47:03 +0100

    Removed useless box in clanwars

--- www/clanwars/index.php
+++ www/clanwars/index.php
@@ -4,11 +4,8 @@ require("../render_game.inc.php");
 
 $clanwars = json_decode(file_get_contents('http://woop.ac:81/ActionFPS-PHP-Iterator/api/clanwars.php?count=50'), true);
 ?>
-<article id="questions">
-    <h2>Recent Clanwars</h2>
-    <p>Clan Achievements here ?</p>
-</article>
 <div id="games">
+    <br /><br />
     <?php foreach($clanwars as $war) render_war($war); ?>
 </div>
 <?php echo $foot;
