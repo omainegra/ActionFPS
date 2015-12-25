fdda0890b889d923a8f790048360dc771121338d
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2015-12-25 01:12:42 +0100

    Use already defined containers until CSS proper merge

--- www/clans/index.php
+++ www/clans/index.php
@@ -4,7 +4,7 @@ require("../render_game.inc.php");
 
 $clans = json_decode(file_get_contents('http://api.actionfps.com/clans/'), true);
 ?>
-<article id="clans">
+<article id="players">
    <ol>
        <?php foreach($clans as $clan) { ?>
             <li>
