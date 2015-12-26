dbafb9779a48f8485d82d573bbe8f5f8bc93214b
William Narmontas <william@scalawilliam.com>
2015-12-26 17:00:34 +0000

    Report ay failures

--- api/app/services/GamesService.scala
+++ api/app/services/GamesService.scala
@@ -5,10 +5,11 @@ import javax.inject._
 import acleague.enrichers.JsonGame
 import af.EnrichGames
 import akka.agent.Agent
-import play.api.Configuration
+import play.api.{Logger, Configuration}
 import play.api.inject.ApplicationLifecycle
 
 import scala.concurrent.ExecutionContext
+import scala.util.control.NonFatal
 
 @Singleton
 class GamesService @Inject()(val configuration: Configuration,
@@ -18,12 +19,22 @@ class GamesService @Inject()(val configuration: Configuration,
                             (implicit executionContext: ExecutionContext)
   extends TailsGames {
 
+  val logger = Logger(getClass)
+
+  logger.info("Starting games service...")
   val allGames: Agent[List[JsonGame]] = Agent(List.empty)
 
   override def processGame(game: JsonGame): Unit = {
     val er = EnrichGames(recordsService.users, recordsService.clans)
     import er.withUsersClass
-    allGames.alter(list => list :+ game.withoutHosts.withUsers.flattenPlayers.withClans)
+    try {
+      val newGame = game.withoutHosts.withUsers.flattenPlayers.withClans
+      allGames.alter(list => list :+ newGame)
+    }
+    catch {
+      case NonFatal(e) =>
+        logger.error(s"Failed to process game $game due to $e", e)
+    }
   }
 
   initialiseTailer(fromStart = true)
