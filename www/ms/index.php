3dac2c781a49cfbe7eb988535df807888005b69d
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2015-12-28 17:39:19 +0100

    Masterserver for ActionFPS clients

--- www/ms/index.php
+++ www/ms/index.php
@@ -1,1 +1,6 @@
-Here it is
+<?php
+$servers = json_decode(file_get_contents('http://api.actionfps.com/servers/'), true);
+
+foreach($servers as $server) { ?>
+addserver <?php echo $server['hostname'] ?> <?php echo $server['port'] ?>
+<?php } ?>
