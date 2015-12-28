29b41746244d4d158ff480e88da67125ecc869e6
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2015-12-28 17:44:15 +0100

    MS server list item separation fix

--- www/ms/index.php
+++ www/ms/index.php
@@ -2,5 +2,6 @@
 $servers = json_decode(file_get_contents('http://api.actionfps.com/servers/'), true);
 
 foreach($servers as $server) { ?>
-addserver <?php echo $server['hostname'] ?> <?php echo $server['port'] ?>
+addserver <?php echo $server['hostname'] ?> <?php echo $server['port'] ?>;
+
 <?php } ?>
