1c3674244483e8b4c7d52ee1d2924f4bbfb14960
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2015-12-28 22:57:23 +0100

    Get server pass from spreadsheet

--- reference-reader/src/main/scala/af/rr/ServerRecord.scala
+++ reference-reader/src/main/scala/af/rr/ServerRecord.scala
@@ -11,7 +11,7 @@ import scala.util.Try
   * Created by William on 05/12/2015.
   */
 
-case class ServerRecord(region: String, hostname: String, port: Int, kind: String)
+case class ServerRecord(region: String, hostname: String, port: Int, kind: String, password: Option[String])
 
 object ServerRecord {
   def parseRecords(input: Reader): List[ServerRecord] = {
@@ -22,11 +22,13 @@ object ServerRecord {
         hostname <- Option(rec.get(1)).filter(_.nonEmpty)
         port <- Try(rec.get(2).toInt).toOption
         kind <- Option(rec.get(3)).filter(_.nonEmpty)
+        password <- Option(rec.get(4)).filter(_.nonEmpty)
       } yield ServerRecord(
         region = region,
         hostname = hostname,
         port = port,
-        kind = kind
+        kind = kind,
+        password = Option(password)
       )
     }.toList
 
