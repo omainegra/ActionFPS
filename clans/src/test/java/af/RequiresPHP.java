1c19101c0e24e2965afa12d1de0f5b3f10622fe9
William Narmontas <william@scalawilliam.com>
2016-01-01 10:00:57 +0000

    Remove stuff

--- clans/src/test/java/af/RequiresPHP.java
+++
@@ -1,9 +0,0 @@
-package af;
-import java.lang.annotation.*;
-
-@org.scalatest.TagAnnotation
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD, ElementType.TYPE})
-public @interface RequiresPHP {
-
-}
