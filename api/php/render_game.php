34b9b3b6478c2c45a84dcecf9721e03cfcea5a6e
William Narmontas <william@scalawilliam.com>
2016-01-08 06:19:41 +0000

    Remove API. Don't need it any longer.

--- api/php/render_game.php
+++
@@ -1,7 +0,0 @@
-<?php
-ini_set("display_errors", "Off");
-require "render_game.inc.php";
-$game = json_decode(file_get_contents("php://input"), true);
-if ( isset($game['map']) ) {
-    render_game($game);
-}
