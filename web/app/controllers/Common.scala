f1d36291f06fd4d104be1764d65f3e8c82d50308
Vykintas Narmontas (William) <william@scalawilliam.com>
2016-04-10 15:40:38 +0800

    Add Localisation for French & Portuguese

--- web/app/controllers/Common.scala
+++ web/app/controllers/Common.scala
@@ -10,7 +10,9 @@ import javax.inject._
 
 import groovy.json.JsonSlurper
 import groovy.text.markup.{TemplateResolver, MarkupTemplateEngine, TemplateConfiguration}
+import org.jsoup.Jsoup
 import play.api.Configuration
+import play.api.i18n.{I18nSupport, MessagesApi, Messages}
 import play.api.libs.json.{Json, JsValue}
 import play.api.libs.ws.{WSResponse, WSRequest, WSClient}
 import play.api.mvc.{Result, RequestHeader, AnyContent, Action}
@@ -20,18 +22,19 @@ import scala.async.Async._
 import play.api.mvc.Results._
 import scala.concurrent.{Future, ExecutionContext}
 
-class Common @Inject()(configuration: Configuration)(implicit wsClient: WSClient,
-                                                     executionContext: ExecutionContext) {
+class Common @Inject()(configuration: Configuration
+                      )(implicit wsClient: WSClient,
+                        val messagesApi: MessagesApi, executionContext: ExecutionContext) extends I18nSupport {
 
 
   def renderTemplate(title: Option[String], supportsJson: Boolean, login: Option[(String, String)])(html: Html)
                     (implicit requestHeader: RequestHeader) = {
     import org.jsoup.Jsoup
     var f = new File("web/dist/www/template.html")
-    if ( !f.exists()) {
+    if (!f.exists()) {
       f = new File("www/template.html")
     }
-    if ( !f.exists() ) {
+    if (!f.exists()) {
       f = new File("dist/www/template.html")
     }
     val js = Jsoup.parse(f, "UTF-8")
@@ -39,6 +42,10 @@ class Common @Inject()(configuration: Configuration)(implicit wsClient: WSClient
     if (supportsJson) {
       js.select("#content").attr("data-has-json", "has-json")
     }
+
+    js.select(s"#reg-menu-reg-play").first().text(Messages("menu.register-play"))
+    js.select(s"#reg-menu-play").first().text(Messages("menu.play"))
+
     PartialFunction.condOpt(requestHeader.cookies.get("af_id").map(_.value) -> requestHeader.cookies.get("af_name").map(_.value)) {
       case (Some(id), Some(name)) =>
         js.select("#log-in").first().text(name)
@@ -49,6 +56,10 @@ class Common @Inject()(configuration: Configuration)(implicit wsClient: WSClient
         js.select("#reg-menu-play").parents().first().remove()
     }
     js.select("#content").html(html.body)
+
+    Set("clan-ranks", "clan-wars", "official-clans", "player-ranks", "hall-of-fame", "servers", "faq", "forum", "login").foreach {
+      id => js.select(s"#menu-${id} a").first().text(Messages(s"menu.${id}"))
+    }
     Html(js.toString)
   }
 
@@ -68,7 +79,17 @@ class Common @Inject()(configuration: Configuration)(implicit wsClient: WSClient
       }
       val rendered = await(f(wsClient.url(s"$mainPath$path")
         .withQueryString(extraParams: _*))).body
-      Ok(Html(rendered.cleanupPaths))
+      val parsedHtml = Jsoup.parse(rendered.cleanupPaths)
+      val content = parsedHtml.select("#content")
+      if ( content.size() == 1 ) {
+        val gotIt = renderTemplate(
+          title = Option(parsedHtml.select("title")).map(_.text),
+          supportsJson = false,
+          login = None)(Html(content.html()))
+        Ok(gotIt)
+      } else {
+        Ok(Html(rendered.cleanupPaths))
+      }
     }
   }
 
@@ -78,9 +99,10 @@ class Common @Inject()(configuration: Configuration)(implicit wsClient: WSClient
 
   def forward(path: String, id: String): Action[AnyContent] = forward(path, Option(id))
 
-  def forward(path: String, id: Option[String] = None): Action[AnyContent] = Action.async { implicit request =>
-    renderPhp(path)(_.withQueryString(id.map(i => "id" -> i).toList: _*).get())
-  }
+  def forward(path: String, id: Option[String] = None): Action[AnyContent] =
+    Action.async { implicit request =>
+      renderPhp(path)(_.withQueryString(id.map(i => "id" -> i).toList: _*).get())
+    }
 
 
 }
