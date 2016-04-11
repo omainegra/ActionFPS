79802991fe042203e69c4d1c5399228552988564
William Narmontas <william@scalawilliam.com>
2016-04-11 13:13:07 +0800

    Add more checking

--- web/app/providers/games/CachedJournalGamesProvider.scala
+++ web/app/providers/games/CachedJournalGamesProvider.scala
@@ -95,7 +95,7 @@ class CachedJournalGamesProvider @Inject()(configuration: Configuration,
   def addWriteShutdownHook(): Unit = {
     applicationLifecycle.addStopHook(() => Future {
       concurrent.blocking {
-        Logger.info(s"Shutting down ${getClass.getSimpleName}")
+        Logger.info(s"Shutting down ${getClass.getSimpleName}: have games ${games.value.flatMap(_.toOption).map(_.size)}")
         games.value.flatMap(_.toOption).foreach { result =>
           Logger.info(s"Saving game cache for ${result.size} games...")
           val fos = new FileOutputStream(targetFile)
