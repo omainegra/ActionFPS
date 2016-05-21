8765a05fc7ff7d2bbd16d0cb261d3db2dbb1a5a1
Vykintas Narmontas (William) <william@scalawilliam.com>
2016-05-21 15:17:46 +0800

    Add some more links

--- web/dist/www/contact/index.php
+++ web/dist/www/contact/index.php
@@ -4,8 +4,12 @@ require("../render_game.inc.php");
 ?>
 <article id="questions">
     <h1>Contact</h1>
-    <h2><a href="https://docs.google.com/forms/d/10DqKjc8YxnpQ7llI2E2AT4ceYtypzZg0d5LD4lRTe0E/viewform">Change nickname form</a></h2>
+    <h2><a href="https://docs.google.com/forms/d/10DqKjc8YxnpQ7llI2E2AT4ceYtypzZg0d5LD4lRTe0E/viewform">Change nickname
+            form</a></h2>
     <p>Request a nickname change for an existing user.</p>
+    <h2>Other questions</h2>
+    <p>E-mail: <a href="mailto:actionfps@googlegroups.com">actionfps@googlegroups.com</a> (<a
+            href="https://groups.google.com/forum/#!aboutgroup/actionfps" target="_blank">ActionFPS Forum</a>)</p>
 </article>
 
 <?php echo $foot; ?>
