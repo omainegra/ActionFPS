dca1ea26ef7e15484e06675ca67085d8a6fab8d1
Vykintas Narmontas (William) <william@scalawilliam.com>
2016-05-29 09:06:36 +0800

    Make Stats page more useful

--- stats/src/main/scala/com/actionfps/stats/Stats.scala
+++ stats/src/main/scala/com/actionfps/stats/Stats.scala
@@ -3,41 +3,34 @@ package com.actionfps.stats
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
+import scala.collection.immutable.ListMap
 import scala.xml.Elem
 
 /**
   * Created by me on 22/04/2016.
   */
 object Stats {
+  private val normaliseHourShift = -3
 
-  def idsToTableRows(ids: List[String], width: Double, height: Double, r: Double): List[Elem] = {
-    implicit val ordering = Ordering.by[ZonedDateTime, Double](_.toEpochSecond)
-    val horizontalPadding = r
-    ids.map(ZonedDateTime.parse)
-      .groupBy(_.withHour(0).withMinute(0).withSecond(0).withNano(0))
-      .toList.sortBy(_._1).reverse
-      .map { case (header, items) =>
-        <tr>
-          <th>
-            {header.format(DateTimeFormatter.ofPattern("d MMM yy"))}
-          </th>
-          <td>
-            <svg width={s"${width + 2 * horizontalPadding}"} height={s"$height"}>
-              <g>
-                {items.map { item =>
-                val cx = width * (item.toEpochSecond - header.toEpochSecond) / (24 * 3600)
-                <a xlink:href={s"/game/?id=$item"}
-                   xhref:title={s"Game $item"}>
-                  <circle
-                  cy={s"${height / 2}"} r={s"$r"}
-                  cx={s"${cx + horizontalPadding}"}/>
-                </a>
-              }}
-              </g>
-            </svg>
-          </td>
-        </tr>
-      }
+  case class GameCounter(dates: ListMap[ZonedDateTime, Int]) {
+    def include(id: String): GameCounter = {
+      val parsedDay = ZonedDateTime.parse(id)
+        .plusHours(normaliseHourShift)
+        .withHour(0)
+        .withMinute(0)
+        .withSecond(0)
+        .withNano(0)
+      GameCounter(
+        dates = dates.updated(parsedDay, dates.getOrElse(parsedDay, 0) + 1)
+      )
+    }
+    def take(n: Int): GameCounter = GameCounter(
+      dates = dates.takeRight(n)
+    )
+  }
+
+  object GameCounter {
+    def empty: GameCounter = GameCounter(dates = ListMap.empty)
   }
 
 }
--- web/app/controllers/StatsController.scala
+++ web/app/controllers/StatsController.scala
@@ -1,15 +1,18 @@
 package controllers
 
-import javax.inject.{Inject, Singleton}
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import javax.inject.Inject
 
 import com.actionfps.stats.Stats
 import play.api.Configuration
+import play.api.libs.json._
 import play.api.libs.ws.WSClient
 import play.api.mvc.{Action, Controller}
-import play.twirl.api.Html
 import providers.ReferenceProvider
 import providers.full.FullProvider
 
+import scala.collection.immutable.ListMap
 import scala.concurrent.ExecutionContext
 
 /**
@@ -24,27 +27,30 @@ class StatsController @Inject()(common: Common,
 
   def stats = Action.async { implicit request =>
     import scala.async.Async._
+
     async {
+      val gc = await(fullProvider.allGames).map(_.id).foldLeft(Stats.GameCounter.empty)(_.include(_))
+        .take(366)
       Ok(common.renderTemplate(
         title = Option("Stats"),
         supportsJson = false,
         login = None
       )(
-        html = Html(
-          <div id="stats">
-            <table class="listing">
-              <tbody>
-                {Stats.idsToTableRows(
-                ids = await(fullProvider.allGames).map(_.id),
-                width = 540,
-                height = 20,
-                r = 4
-              )}
-              </tbody>
-            </table>
-          </div>.toString)
+        html = views.html.stats.gameCounter(gc)
       ))
     }
   }
 
+}
+
+object StatsController {
+  val fmt = DateTimeFormatter.ISO_DATE
+  implicit val lmWriter = Writes[ListMap[ZonedDateTime, Int]](pgc =>
+    JsArray(pgc.map {
+      case (d, n) => JsObject(Map(
+        "date" -> JsString(fmt.format(d)),
+        "count" -> JsNumber(n)
+      ))
+    }.toList)
+  )
 }
