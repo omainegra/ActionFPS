4d791a470cefe299b34b8ed929d41d999e3d1585
Vykintas Narmontas (William) <william@scalawilliam.com>
2017-01-17 23:03:33 +0800

    Address https://github.com/ScalaWilliam/ActionFPS/pull/260#discussion_r96428312

--- inter-parser/src/main/scala/com/actionfps/inter/LastCallRecord.scala
+++ inter-parser/src/main/scala/com/actionfps/inter/LastCallRecord.scala
@@ -5,7 +5,9 @@ import java.time.Instant
 /**
   * Created by me on 17/01/2017.
   */
-case class LastCallRecord(user: Map[String, Instant], server: Map[String, Instant]) {
+case class LastCallRecord(user: Map[String, Instant],
+                          server: Map[String, Instant],
+                          ip: Map[String, Instant]) {
   /** Return if updated <=> had effect, so emit a record **/
   def include(interOut: InterOut): Option[LastCallRecord] = {
     val acceptByUser = user.get(interOut.user) match {
@@ -18,9 +20,15 @@ case class LastCallRecord(user: Map[String, Instant], server: Map[String, Instan
       case Some(latestServerInstant) =>
         interOut.instant.minus(java.time.Duration.ofMinutes(5)).isAfter(latestServerInstant)
     }
-    if (acceptByServer && acceptByUser) Option {
+    val acceptByIp = ip.get(interOut.ip) match {
+      case None => true
+      case Some(latestIpInstant) =>
+        interOut.instant.minus(java.time.Duration.ofMinutes(5)).isAfter(latestIpInstant)
+    }
+    if (acceptByServer && acceptByUser && acceptByIp) Option {
       LastCallRecord(
         user = user.updated(interOut.user, interOut.instant),
+        ip = ip.updated(interOut.ip, interOut.instant),
         server = server.updated(interOut.serverName, interOut.instant)
       )
     } else None
@@ -30,6 +38,7 @@ case class LastCallRecord(user: Map[String, Instant], server: Map[String, Instan
 object LastCallRecord {
   val empty: LastCallRecord = LastCallRecord(
     user = Map.empty,
-    server = Map.empty
+    server = Map.empty,
+    ip = Map.empty
   )
 }
