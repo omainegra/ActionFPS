8ba351c3af585e48738812c21a667c57b580c1d1
Vykintas Narmontas (William) <me@vynar.com>
2017-01-18 21:08:44 +0800

    Travis it (#267)
    * Spaace
    
    * Remove unnecessary part. Makefile to run it:test.
    
    * Try sudo:true
    
    * Slim down the build a bit more.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,34 +4,24 @@ object Dependencies {
 
   val scalatest: ModuleID = "org.scalatest" %% "scalatest" % "3.0.1"
   val scalatestOld: ModuleID = "org.scalatest" %% "scalatest" % "2.2.6"
-  val json4s: ModuleID = "org.json4s" %% "json4s-jackson" % "3.4.2"
-  val fastParse: ModuleID = "com.lihaoyi" %% "fastparse" % "0.4.2"
   val async: ModuleID = "org.scala-lang.modules" %% "scala-async" % "0.9.6"
-  val xml: ModuleID = "org.scala-lang.modules" %% "scala-xml" % "1.0.6"
-  val commonsNet: ModuleID = "commons-net" % "commons-net" % "3.5"
-  val jodaTime: ModuleID = "joda-time" % "joda-time" % "2.9.6"
-  val jodaConvert: ModuleID = "org.joda" % "joda-convert" % "1.8.1"
   val commonsCsv: ModuleID = "org.apache.commons" % "commons-csv" % "1.4"
   val kantanCsv: ModuleID = "com.nrinaudo" %% "kantan.csv-generic" % "0.1.15"
   val jsoup: ModuleID = "org.jsoup" % "jsoup" % "1.10.1"
   val hazelcastClient: ModuleID = "com.hazelcast" % "hazelcast-client" % "3.6.5"
   val fluentHc: ModuleID = "org.apache.httpcomponents" % "fluent-hc" % "4.5.2"
   val httpClientCache: ModuleID = "org.apache.httpcomponents" % "httpclient-cache" % "4.5.2"
-  val commonsIo: ModuleID = "commons-io" % "commons-io" % "2.5"
   val scalatestPlus: ModuleID = "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1"
-  val alpakkaFile: ModuleID = "com.lightbend.akka" %% "akka-stream-alpakka-file" % "0.4"
+  val alpakkaFile: ModuleID = "com.lightbend.akka" %% "akka-stream-alpakka-file" % "0.5"
   val seleniumJava: ModuleID = "org.seleniumhq.selenium" % "selenium-java" % "2.53.1"
   val seleniumHtmlUnit: ModuleID = "org.seleniumhq.selenium" % "selenium-htmlunit-driver" % "2.52.0"
 
   val geoipApi: ModuleID = "com.maxmind.geoip" % "geoip-api" % "1.3.1"
-  val shapeless: ModuleID = "com.chuusai" %% "shapeless" % "2.3.2"
 
   val akkaActor: ModuleID = akka("actor")
   val akkaAgent: ModuleID = akka("agent")
   val akkaslf: ModuleID = akka("slf4j")
   val akkaTestkit: ModuleID = akka("testkit")
-
-  val mockito: ModuleID = "org.mockito" % "mockito-all" % "1.10.19"
 
   private def akka(stuff: String) = "com.typesafe.akka" %% s"akka-$stuff" % "2.4.14"
 
@@ -40,10 +30,5 @@ object Dependencies {
   val serverPinger: ModuleID = "com.actionfps" %% "server-pinger" % "5.4.1"
   val gameParser: ModuleID = "com.actionfps" %% "game-parser" % "5.6.0"
   val pureGame: ModuleID = "com.actionfps" %% "pure-game" % "5.6.0"
-
-  val scalaWilliamResolver: MavenRepository = Resolver.bintrayRepo("scalawilliam", "maven")
-  val actionFpsResolver: MavenRepository = Resolver.bintrayRepo("actionfps", "maven")
-
-
 
 }
