2c6aa5129afecdb1cb24d25842fceb37ecad9c26
Vykintas Narmontas (William) <william@scalawilliam.com>
2017-01-19 12:48:58 +0800

    New games to go through event stream and not game provider. Fix #269.

--- web/app/providers/games/NewGamesProvider.scala
+++ web/app/providers/games/NewGamesProvider.scala
@@ -3,52 +3,45 @@ package games
 
 import javax.inject.{Inject, Singleton}
 
-import akka.NotUsed
 import akka.actor.ActorSystem
+import akka.stream.OverflowStrategy
 import akka.stream.scaladsl.Source
 import com.actionfps.formats.json.Formats._
 import com.actionfps.gameparser.enrichers.JsonGame
 import lib.WebTemplateRender
-import play.api.inject.ApplicationLifecycle
 import play.api.libs.EventSource.Event
-import play.api.libs.iteratee.Concurrent
 import play.api.libs.json.{JsBoolean, JsObject, JsString, Json}
-import play.api.libs.streams.Streams
-import play.api.{Configuration, Logger}
+import providers.full.NewGameDetected
 import views.rendergame.MixedGame
 
-import scala.concurrent.{ExecutionContext, Future}
-import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext
 
 /**
   * Created by William on 09/12/2015.
   */
 @Singleton
-class NewGamesProvider @Inject()(applicationLifecycle: ApplicationLifecycle,
-                                 common: WebTemplateRender,
-                                 gamesProvider: GamesProvider)
+class NewGamesProvider @Inject()(common: WebTemplateRender)
                                 (implicit actorSystem: ActorSystem,
                                  executionContext: ExecutionContext) {
 
-  private val (newGamesEnum, thing) = Concurrent.broadcast[Event]
-  val newGamesSource: Source[Event, NotUsed] = Source.fromPublisher(Streams.enumeratorToPublisher(newGamesEnum))
+  val newGamesSource: Source[Event, Boolean] = {
+    Source
+      .actorRef[NewGameDetected](10, OverflowStrategy.dropHead)
+      .mapMaterializedValue(actorSystem.eventStream.subscribe(_, classOf[NewGameDetected]))
+      .map(_.jsonGame)
+      .map(gameToEvent)
+  }
 
-  private val logger = Logger(getClass)
-  logger.info("Starting new games provider")
-
-  def processGame(game: JsonGame): Unit = {
+  private def gameToEvent(game: JsonGame): Event = {
     val b = Json.toJson(game.withoutHosts).asInstanceOf[JsObject].+("isNew" -> JsBoolean(true))
 
     val gameHtml = views.rendergame.Render.renderMixedGame(MixedGame.fromJsonGame(game))
-    thing.push(
-      Event(
-        id = Option(game.id),
-        name = Option("new-game"),
-        data = Json.toJson(b).asInstanceOf[JsObject].+("html" -> JsString(gameHtml.body)).toString()
-      )
+    Event(
+      id = Option(game.id),
+      name = Option("new-game"),
+      data = Json.toJson(b).asInstanceOf[JsObject].+("html" -> JsString(gameHtml.body)).toString()
     )
   }
 
-  gamesProvider.addAutoRemoveHook(applicationLifecycle)(processGame)
 
 }
