00830e44701cf130341c00df0536c13461a44ed6
William Narmontas <me@vynar.com>
2017-05-09 20:22:02 +0800

    Add batch log file sync script

---
+++ log-server/actionfps-log-sync.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+usage() {
+    echo "Usage: $0 [target TSV file]"
+    echo 
+    echo Synchronises ActionFPS logs periodically.
+    echo 
+    echo CURL_OPTS environment variable is available.
+    echo If file does not exist, try to create it.
+}
+CURL_OPTS="-s"
+set -e
+[ -z "$1" ] && { usage; exit 1; }
+[ "$1" == "" ] && { usage; exit 0; }
+TARGET_FILE="$1"
+touch ${TARGET_FILE} || { echo "Failed to touch file ${TARGET_FILE}" ; exit 1; }
+START_TIME=$(grep -v '^$' ${TARGET_FILE} | tail -n 1 | cut -f 1 -d $'\t')
+START_TIME_REPETITIONS=$(tail -n 100 ${TARGET_FILE} | grep -F ${START_TIME} | wc -l)
+START_TIME=${START_TIME:=2014-12-01T01:01:01Z}
+echo Start time: ${START_TIME} >> /dev/stderr;
+END_TIME=2099-04-10T11:02:03Z
+DOWNLOAD_URL="https://actionfps.com/logs.tsv?from=${START_TIME}&to=${END_TIME}"
+curl --fail ${CURL_OPTS} ${DOWNLOAD_URL} | \
+    tail -n +$[ START_TIME_REPETITIONS + 1 ] | \
+    grep -v -F ${START_TIME} | \
+    tee -a ${TARGET_FILE} | \
+    awk '{ c++ } ; END{ print c " lines added" >> "/dev/stderr"; }'
