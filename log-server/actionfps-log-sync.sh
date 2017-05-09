5517c2015c83852a7ac4bd3dc6efbe5f90621733
William Narmontas <me@vynar.com>
2017-05-09 20:24:21 +0800

    Make it a bit pretties

--- log-server/actionfps-log-sync.sh
+++ log-server/actionfps-log-sync.sh
@@ -12,13 +12,22 @@ set -e
 [ -z "$1" ] && { usage; exit 1; }
 [ "$1" == "" ] && { usage; exit 0; }
 TARGET_FILE="$1"
+
 touch ${TARGET_FILE} || { echo "Failed to touch file ${TARGET_FILE}" ; exit 1; }
+
+# Get start time from the last line of the file
 START_TIME=$(grep -v '^$' ${TARGET_FILE} | tail -n 1 | cut -f 1 -d $'\t')
+
+# Get the number of times this time was repeated - so we could skip this many
+# lines for that time when resuming
 START_TIME_REPETITIONS=$(tail -n 100 ${TARGET_FILE} | grep -F ${START_TIME} | wc -l)
 START_TIME=${START_TIME:=2014-12-01T01:01:01Z}
-echo Start time: ${START_TIME} >> /dev/stderr;
 END_TIME=2099-04-10T11:02:03Z
+
+echo Resume from time: ${START_TIME} >> /dev/stderr;
+
 DOWNLOAD_URL="https://actionfps.com/logs.tsv?from=${START_TIME}&to=${END_TIME}"
+
 curl --fail ${CURL_OPTS} ${DOWNLOAD_URL} | \
     tail -n +$[ START_TIME_REPETITIONS + 1 ] | \
     grep -v -F ${START_TIME} | \
