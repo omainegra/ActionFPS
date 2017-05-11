b16bfc84c6e86042368b381c372807ae75708c3f
William Narmontas <me@vynar.com>
2017-05-11 15:42:30 +0800

    Access the large journal too.

--- web/app/controllers/LargeJournalController.scala
+++ web/app/controllers/LargeJournalController.scala
@@ -1,17 +1,16 @@
 package controllers
 
-import java.nio.file.Path
+import java.nio.file.{Path, Paths}
 import javax.inject.{Inject, Singleton}
 
-import lib.ForJournal
 import play.api.Configuration
 
 /**
   * Created by me on 02/05/2017.
   */
 @Singleton
-class RawLogController(logFile: Path) extends LogController(logFile: Path) {
+class LargeJournalController(logFile: Path) extends LogController(logFile: Path) {
   @Inject() def this(configuration: Configuration) = this(
-    ForJournal.ForConfig(configuration.underlying).lastLogPathO.get
+    Paths.get(configuration.underlying.getString("journal.large"))
   )
 }
