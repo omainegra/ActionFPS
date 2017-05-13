b92511235fa9ca85915ddec395e5de966ca6478d
William Narmontas <me@vynar.com>
2017-05-13 15:20:54 +0800

    #380: Reduce allocation due to server check.
    This was using a HashSet which would cause an Iterator to be created.
    A List is more efficient.
    Down to 00:44.

--- ladder/ladder-parser/src/main/scala/com/actionfps/ladder/parser/TsvExtract.scala
+++ ladder/ladder-parser/src/main/scala/com/actionfps/ladder/parser/TsvExtract.scala
@@ -7,23 +7,26 @@ import java.time.Instant
   */
 case class TsvExtract(servers: Set[String], nickToUser: NickToUser) {
   private val regex = s"""\\[([^\\]]+)\\] ([^ ]+) (.*)""".r
+  private val serversList = servers.toList
   def unapply(line: String): Option[(String, TimedUserMessage)] = {
-    if (servers.exists(server => line.contains(server))) {
+    if (serversList.exists(server => line.contains(server))) {
       val splittedLine = line.split('\t')
       if (splittedLine.length >= 3) {
         val server = splittedLine(1)
         if (servers.contains(server)) {
           val message = splittedLine(2)
-          message match {
-            case regex(ip, nick, content) =>
-              val timestamp = Instant.parse(splittedLine(0))
-              nickToUser.userOfNickname(nick, timestamp) match {
-                case Some(uuser) =>
-                  Some(server -> TimedUserMessage(timestamp, uuser, content))
-                case None => None
-              }
-            case _ => None
-          }
+          if (message.length > 0 && message.charAt(0) == '[') {
+            message match {
+              case regex(ip, nick, content) =>
+                val timestamp = Instant.parse(splittedLine(0))
+                nickToUser.userOfNickname(nick, timestamp) match {
+                  case Some(uuser) =>
+                    Some(server -> TimedUserMessage(timestamp, uuser, content))
+                  case None => None
+                }
+              case _ => None
+            }
+          } else None
         } else None
       } else None
     } else None
