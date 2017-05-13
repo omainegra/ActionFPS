d290c46fc66ce98adeb3e2996b740924c649c330
William Narmontas <me@vynar.com>
2017-05-13 09:39:23 +0800

    43 --> 28s

--- players/player-stats/src/main/scala/com/actionfps/players/PlayerGameCounts.scala
+++ players/player-stats/src/main/scala/com/actionfps/players/PlayerGameCounts.scala
@@ -8,18 +8,21 @@ import scala.collection.immutable.ListMap
   * Created by me on 26/05/2016.
   */
 object PlayerGameCounts {
-  def empty: PlayerGameCounts = PlayerGameCounts(games = List.empty, counts = ListMap.empty)
+  def empty: PlayerGameCounts =
+    PlayerGameCounts(games = List.empty, counts = ListMap.empty)
 }
 
-case class PlayerGameCounts(games: List[ZonedDateTime], counts: ListMap[ZonedDateTime, Int]) {
+case class PlayerGameCounts(games: List[Instant],
+                            counts: ListMap[ZonedDateTime, Int]) {
   def gamesSince(instant: Instant): Int = {
-    games.count(_.toInstant.isAfter(instant))
+    games.count(_.isAfter(instant))
   }
 
   def include(zonedDateTime: ZonedDateTime): PlayerGameCounts = {
-    val dateKey = zonedDateTime.withHour(0).withMinute(0).withSecond(0).withNano(0)
+    val dateKey =
+      zonedDateTime.withHour(0).withMinute(0).withSecond(0).withNano(0)
     copy(
-      games = games :+ zonedDateTime,
+      games = games :+ zonedDateTime.toInstant,
       counts = counts.updated(dateKey, counts.getOrElse(dateKey, 0) + 1)
     )
   }
