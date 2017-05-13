3210f1cfec54aecf88119391b076d803808fdedc
William Narmontas <me@vynar.com>
2017-05-13 14:50:51 +0800

    #380: Prefilter servers & don't merge eagerly.
    03:10 --> 00:55

--- ladder/ladder-parser/src/main/scala/com/actionfps/ladder/parser/TsvExtract.scala
+++ ladder/ladder-parser/src/main/scala/com/actionfps/ladder/parser/TsvExtract.scala
@@ -7,21 +7,23 @@ import java.time.Instant
   */
 case class TsvExtract(servers: Set[String], nickToUser: NickToUser) {
   def unapply(line: String): Option[(String, TimedUserMessage)] = {
-    val splittedLine = line.split("\t", -1)
-    val server = splittedLine(1)
-    if (splittedLine.length >= 3 && servers.contains(server)) {
-      val message = splittedLine(2)
-      val regex = s"""\\[([^\\]]+)\\] ([^ ]+) (.*)""".r
-      message match {
-        case regex(ip, nick, content) =>
-          val timestamp = Instant.parse(splittedLine(0))
-          nickToUser.userOfNickname(nick, timestamp) match {
-            case Some(uuser) =>
-              Some(server -> TimedUserMessage(timestamp, uuser, content))
-            case None => None
-          }
-        case _ => None
-      }
+    if (servers.exists(server => line.contains(server))) {
+      val splittedLine = line.split("\t", -1)
+      val server = splittedLine(1)
+      if (splittedLine.length >= 3 && servers.contains(server)) {
+        val message = splittedLine(2)
+        val regex = s"""\\[([^\\]]+)\\] ([^ ]+) (.*)""".r
+        message match {
+          case regex(ip, nick, content) =>
+            val timestamp = Instant.parse(splittedLine(0))
+            nickToUser.userOfNickname(nick, timestamp) match {
+              case Some(uuser) =>
+                Some(server -> TimedUserMessage(timestamp, uuser, content))
+              case None => None
+            }
+          case _ => None
+        }
+      } else None
     } else None
   }
 }
