f37ba95c0019b1cbcef99bfc34de4939d7dbc0b5
William Narmontas <me@vynar.com>
2017-05-13 15:02:02 +0800

    #380: Reduce regex allocation.
    Down to 00:46.

--- ladder/ladder-parser/src/main/scala/com/actionfps/ladder/parser/TsvExtract.scala
+++ ladder/ladder-parser/src/main/scala/com/actionfps/ladder/parser/TsvExtract.scala
@@ -6,23 +6,25 @@ import java.time.Instant
   * Created by me on 11/05/2017.
   */
 case class TsvExtract(servers: Set[String], nickToUser: NickToUser) {
+  private val regex = s"""\\[([^\\]]+)\\] ([^ ]+) (.*)""".r
   def unapply(line: String): Option[(String, TimedUserMessage)] = {
     if (servers.exists(server => line.contains(server))) {
-      val splittedLine = line.split("\t", -1)
-      val server = splittedLine(1)
-      if (splittedLine.length >= 3 && servers.contains(server)) {
-        val message = splittedLine(2)
-        val regex = s"""\\[([^\\]]+)\\] ([^ ]+) (.*)""".r
-        message match {
-          case regex(ip, nick, content) =>
-            val timestamp = Instant.parse(splittedLine(0))
-            nickToUser.userOfNickname(nick, timestamp) match {
-              case Some(uuser) =>
-                Some(server -> TimedUserMessage(timestamp, uuser, content))
-              case None => None
-            }
-          case _ => None
-        }
+      val splittedLine = line.split('\t')
+      if (splittedLine.length >= 3) {
+        val server = splittedLine(1)
+        if (servers.contains(server)) {
+          val message = splittedLine(2)
+          message match {
+            case regex(ip, nick, content) =>
+              val timestamp = Instant.parse(splittedLine(0))
+              nickToUser.userOfNickname(nick, timestamp) match {
+                case Some(uuser) =>
+                  Some(server -> TimedUserMessage(timestamp, uuser, content))
+                case None => None
+              }
+            case _ => None
+          }
+        } else None
       } else None
     } else None
   }
