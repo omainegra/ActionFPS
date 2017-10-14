a9b59e7e6084205390a62a78f5e29d44f7b4711b
William Narmontas <me@vynar.com>
2017-10-14 18:52:14 +0800

    Revert "Change ApplicationLoader into its own."
    This reverts commit 4dccd066a6e9da1c5ebcf8e422499fb1667a2887.

--- web/app/TheApplicationLoader.scala
+++ web/app/TheApplicationLoader.scala
@@ -1,8 +1,5 @@
-import play.api.{Application, ApplicationLoader}
 import play.api.inject.guice.GuiceApplicationLoader
 
-class TheApplicationLoader extends ApplicationLoader {
-  override def load(context: ApplicationLoader.Context): Application = {
-    new GuiceApplicationLoader().load(context)
-  }
+class TheApplicationLoader extends GuiceApplicationLoader {
+
 }
