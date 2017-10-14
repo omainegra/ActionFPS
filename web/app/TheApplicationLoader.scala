4dccd066a6e9da1c5ebcf8e422499fb1667a2887
ScalaWilliam <william@scalawilliam.com>
2017-10-14 13:45:12 +0800

    Change ApplicationLoader into its own.

--- web/app/TheApplicationLoader.scala
+++ web/app/TheApplicationLoader.scala
@@ -1,5 +1,8 @@
+import play.api.{Application, ApplicationLoader}
 import play.api.inject.guice.GuiceApplicationLoader
 
-class TheApplicationLoader extends GuiceApplicationLoader {
-
+class TheApplicationLoader extends ApplicationLoader {
+  override def load(context: ApplicationLoader.Context): Application = {
+    new GuiceApplicationLoader().load(context)
+  }
 }
