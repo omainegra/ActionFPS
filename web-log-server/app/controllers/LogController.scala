c7ef5ee90961c0ca6273e5c8cb9a52c86f68a40b
ScalaWilliam <william@scalawilliam.com>
2017-10-17 21:28:45 +0800

    Verify against empty signature. Closes #394.

--- web-log-server/app/controllers/LogController.scala
+++ web-log-server/app/controllers/LogController.scala
@@ -23,8 +23,7 @@ import play.api.mvc._
 
 import scala.concurrent.duration._
 
-abstract class LogController(sourceFile: Path,
-                             components: ControllerComponents)
+abstract class LogController(sourceFile: Path, components: ControllerComponents)
     extends AbstractController(components) {
   Logger.info(s"Log controller for ${sourceFile}")
   def stream = Action { request: RequestHeader =>
@@ -91,7 +90,7 @@ abstract class LogController(sourceFile: Path,
   }
 
   def historical(from: String, to: Option[String]) = Action { request =>
-    if (!request.jwtSession.isEmpty()) {
+    if (request.jwtSession.signature != "") {
       require(request.jwtSession.claim.isValid(IssuerName))
     }
     val logAccess: LogAccess = request.jwtSession.claimData
